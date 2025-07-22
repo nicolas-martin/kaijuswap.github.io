f91b35ee4573e19bd9d87c27ada247256dd36c07
Nicolas Martin <nicolas.martin.0986@gmail.com>
2025-07-22 15:57:01 -0400

    try github workflow deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
 	site: 'https://nicolas-martin.github.io',
 	base: '/',
