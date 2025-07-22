5107fb0eb7019c9f1bb9547d066f76cb777224c4
Nicolas Martin <nicolas.martin.0986@gmail.com>
2025-07-22 16:05:12 -0400

    set site to kaijuswap.com and set packagemanager

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-	site: 'https://nicolas-martin.github.io',
+	site: 'https://kaijuswap.com',
 	base: '/',
 	vite: {
 		css: {
