ad2df86fa08b18d0171a3064d980f1f47a06b763
Nicolas Martin <nicolas.martin.0986@gmail.com>
2025-07-23 12:05:10 -0400

    format

--- src/modules/privacyPolicy/index.tsx
+++ src/modules/privacyPolicy/index.tsx
@@ -6,24 +6,24 @@ import { ConfigContext } from "utils/configContext";
 import type { TemplateConfig } from "utils/configType";
 
 interface Props {
-  config: TemplateConfig;
+	config: TemplateConfig;
 }
 
 function PrivacyPolicyPage({ config }: Props) {
-  const { privacyPolicy } = config;
+	const { privacyPolicy } = config;
 
-  return (
-    <ConfigContext.Provider value={config}>
-      <main>
-        <Navbar />
-        <section className="max-w-screen-lg mx-auto py-4 px-4 md:py-16">
-          <Markdown className="prose">{privacyPolicy.content}</Markdown>
-        </section>
-        <AppBanner />
-        <Footer />
-      </main>
-    </ConfigContext.Provider>
-  );
+	return (
+		<ConfigContext.Provider value={config}>
+			<main>
+				<Navbar />
+				<section className="max-w-screen-lg mx-auto py-4 px-4 md:py-16">
+					<Markdown className="prose">{privacyPolicy.content}</Markdown>
+				</section>
+				<AppBanner />
+				<Footer />
+			</main>
+		</ConfigContext.Provider>
+	);
 }
 
 export default PrivacyPolicyPage;
--- src/modules/termsAndConditions/index.tsx
+++ src/modules/termsAndConditions/index.tsx
@@ -6,24 +6,24 @@ import { ConfigContext } from "utils/configContext";
 import type { TemplateConfig } from "utils/configType";
 
 interface Props {
-  config: TemplateConfig;
+	config: TemplateConfig;
 }
 
 function TermsAndConditionsPage({ config }: Props) {
-  const { termsAndConditions } = config;
+	const { termsAndConditions } = config;
 
-  return (
-    <ConfigContext.Provider value={config}>
-      <main>
-        <Navbar />
-        <section className="max-w-screen-lg mx-auto py-4 px-4 md:py-16">
-          <Markdown className="prose">{termsAndConditions.content}</Markdown>
-        </section>
-        <AppBanner />
-        <Footer />
-      </main>
-    </ConfigContext.Provider>
-  );
+	return (
+		<ConfigContext.Provider value={config}>
+			<main>
+				<Navbar />
+				<section className="max-w-screen-lg mx-auto py-4 px-4 md:py-16">
+					<Markdown className="prose">{termsAndConditions.content}</Markdown>
+				</section>
+				<AppBanner />
+				<Footer />
+			</main>
+		</ConfigContext.Provider>
+	);
 }
 
 export default TermsAndConditionsPage;
